ecb328ce1bb1cdd79e01863c5d01e7879f6e6b61
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-12-14 21:39:15 +0100

    Add OptTransform.Wpf

---
+++ JeremyAnsel.Xwa.OptTransform/JeremyAnsel.Xwa.OptTransform.Wpf/OptProfileSelectorDialog.xaml.cs
@@ -0,0 +1,87 @@
+﻿using JeremyAnsel.Xwa.Opt;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace JeremyAnsel.Xwa.OptTransform.Wpf
+{
+    /// <summary>
+    /// Logique d'interaction pour OptProfileSelectorDialog.xaml
+    /// </summary>
+    public partial class OptProfileSelectorDialog : Window
+    {
+        public OptProfileSelectorDialog(string optFileName, bool topmost = false)
+        {
+            InitializeComponent();
+
+            Topmost = topmost;
+
+            this.OptFileName = optFileName;
+
+            OptFile optFile = OptFile.FromFile(optFileName, false);
+
+            this.OptVersions = Enumerable.Range(0, optFile.MaxTextureVersion).ToList();
+            this.OptObjectProfiles = OptTransformHelpers.GetObjectProfiles(OptFileName).Keys.ToList();
+            this.OptSkins = OptTransformHelpers.GetSkins(OptFileName);
+
+            this.DataContext = this;
+        }
+
+        public string OptFileName { get; }
+
+        public List<int> OptVersions { get; }
+
+        public List<string> OptObjectProfiles { get; }
+
+        public List<string> OptSkins { get; }
+
+        public int SelectedVersion { get; set; } = 0;
+
+        public string SelectedObjectProfile { get; set; } = "Default";
+
+        public ObservableCollection<OptSkinItem> SelectedSkins { get; } = new();
+
+        public List<string> SelectedSkinsKeys
+        {
+            get
+            {
+                return SelectedSkins
+                    .Select(t => t.ToString())
+                    .ToList();
+            }
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.DialogResult = true;
+        }
+
+        private void ClearSelectedSkinsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.SelectedSkins.Clear();
+        }
+
+        private void AddSelectedSkinsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.AddSelectedSkin();
+        }
+
+        private void OptSkinsListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.AddSelectedSkin();
+        }
+
+        private void AddSelectedSkin()
+        {
+            if (this.optSkinsListBox.SelectedItem is not string item)
+            {
+                return;
+            }
+
+            OptSkinItem pair = new(item, 100);
+
+            this.SelectedSkins.Add(pair);
+        }
+    }
+}
---
+++ JeremyAnsel.Xwa.OptTransform/JeremyAnsel.Xwa.OptTransform.Wpf/OptSkinItem.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace JeremyAnsel.Xwa.OptTransform.Wpf
+{
+    public sealed class OptSkinItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public OptSkinItem()
+        {
+        }
+
+        public OptSkinItem(string name)
+        {
+            this.name = name;
+        }
+
+        public OptSkinItem(string name, int opacity)
+        {
+            opacity = Math.Min(Math.Max(opacity, 0), 100);
+
+            this.name = name;
+            this.opacity = opacity;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+                }
+            }
+        }
+
+        private int opacity = 100;
+
+        public int Opacity
+        {
+            get
+            {
+                return opacity;
+            }
+
+            set
+            {
+                if (value != opacity)
+                {
+                    value = Math.Min(Math.Max(value, 0), 100);
+
+                    opacity = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Opacity)));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(this.Name);
+
+            if (this.Opacity != 100)
+            {
+                sb.Append('-');
+                sb.Append(this.Opacity.ToString(CultureInfo.InvariantCulture));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
